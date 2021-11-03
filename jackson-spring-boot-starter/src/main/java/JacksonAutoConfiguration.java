f8559f8772a7b0a26a14924d62974c380134d1ab
Daniel Idris <dai@spk.no>
2021-11-03 15:48:50 +0100

    ObjectMapper regiering modules

--- jackson-spring-boot-starter/src/main/java/JacksonAutoConfiguration.java
+++ jackson-spring-boot-starter/src/main/java/JacksonAutoConfiguration.java
@@ -1,4 +1,10 @@
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +16,18 @@ public class JacksonAutoConfiguration {
     @ConditionalOnMissingBean(ObjectMapper.class)
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Configuration
+    public static class RegisterModuleAutoConfiguration {
+        @Autowired
+        private ObjectMapper objectMapper;
+        @Autowired
+        private Collection<Module> modules;
+
+        @PostConstruct
+        public void registerModules() {
+            modules.forEach(objectMapper::registerModule);
+        }
     }
 }
--- spring-boot-under-the-hood/src/main/java/no/bouvet/springbootunderthehood/SpringBootUnderTheHoodApplication.java
+++ spring-boot-under-the-hood/src/main/java/no/bouvet/springbootunderthehood/SpringBootUnderTheHoodApplication.java
@@ -2,8 +2,15 @@ package no.bouvet.springbootunderthehood;
 
 import static java.lang.System.err;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import com.fasterxml.jackson.core.JsonFactoryBuilder;
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,24 +24,28 @@ public class SpringBootUnderTheHoodApplication {
 	}
 
 	@Bean
-	public ObjectMapper objectMapper() {
-		return new ObjectMapper(new JsonFactoryBuilder().quoteChar('|').build());
+	public Module javaTimeModule() {
+		JavaTimeModule javaTimeModule = new JavaTimeModule();
+		javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
+		return javaTimeModule;
 	}
 
 	@Bean
 	public CommandLineRunner commandLineRunner(ObjectMapper objectMapper) {
 		return (args) -> {
-			err.println(objectMapper.writeValueAsString(new Person("Ola", "Nordmann")));
+			err.println(objectMapper.writeValueAsString(new Person("Ola", "Nordmann", LocalDateTime.now())));
 		};
 	}
 
 	public static class Person {
 		public final String firstName;
 		public final String lastName;
+		public final LocalDateTime registered;
 
-		public Person(String firstName, String lastName) {
+		public Person(String firstName, String lastName, LocalDateTime registered) {
 			this.firstName = firstName;
 			this.lastName = lastName;
+			this.registered = registered;
 		}
 	}
 
