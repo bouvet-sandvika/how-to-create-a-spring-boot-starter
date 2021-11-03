e49c1c6f98564cf2644dff456fbd3c01150df19f
Daniel Idris <dai@spk.no>
2021-11-03 14:50:16 +0100

    Initial commit

---
+++ spring-boot-under-the-hood/src/main/java/no/bouvet/springbootunderthehood/SpringBootUnderTheHoodApplication.java
@@ -0,0 +1,13 @@
+package no.bouvet.springbootunderthehood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUnderTheHoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUnderTheHoodApplication.class, args);
+	}
+
+}
---
+++ spring-boot-under-the-hood/src/test/java/no/bouvet/springbootunderthehood/SpringBootUnderTheHoodApplicationTests.java
@@ -0,0 +1,13 @@
+package no.bouvet.springbootunderthehood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnderTheHoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
